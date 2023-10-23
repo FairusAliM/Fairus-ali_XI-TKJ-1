a5c86562de81957c36a4a4b204d94c9f1c46f1e3
FairusAliM <fairusalimuzaki7@gmail.com>
2023-10-23 20:12:46 +0700

    First Commit

--- Fairus ali_praktek_1.py
+++ Fairus ali_praktek_1.py
@@ -1,21 +1,21 @@
-# Membaca total belanjaan pelanggan dari input pengguna
+# nama = Fairus ali muzaki
+# absen = 07
+# kelas = XI-TKJ 1
+# soal = Di sebuah toko online, Anda bertanggung jawab untuk menghitung diskon berdasarkan jumlah total belanjaan pelanggan.
+
 total_belanjaan = float(input("Masukkan total belanjaan pelanggan: "))
 
-# Menghitung diskon berdasarkan aturan
 if total_belanjaan > 500000:
-    diskon = 0.10  # Diskon 10% untuk total belanjaan > 500.000
+    diskon = 0.10 
 elif 300000 <= total_belanjaan <= 500000:
-    diskon = 0.05  # Diskon 5% untuk total belanjaan antara 300.000 dan 500.000
+    diskon = 0.05 
 else:
-    diskon = 0  # Tidak ada diskon untuk total belanjaan kurang dari 300.000
+    diskon = 0  
 
-# Menghitung jumlah diskon
 jumlah_diskon = total_belanjaan * diskon
 
-# Menghitung total yang harus dibayar setelah diskon
 total_setelah_diskon = total_belanjaan - jumlah_diskon
 
-# Menampilkan hasil
 print(f"Total belanjaan: Rp {total_belanjaan:.2f}")
 print(f"Diskon: Rp {jumlah_diskon:.2f}")
 print(f"Total setelah diskon: Rp {total_setelah_diskon:.2f}")
