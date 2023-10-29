8f7dff40d4c0fde394c40822b71715cf1fca6f42
FairusAliM <fairusalimuzaki7@gmail.com>
2023-10-29 18:53:31 +0700

    Ali

--- TUGAS PERCABANGAN/Fairus ali_praktek_2.py
+++ TUGAS PERCABANGAN/Fairus ali_praktek_2.py
@@ -4,17 +4,6 @@
 
 from datetime import datetime
 
-def cek_tepat_waktu(estimasi_selesai, tanggal_target_selesai):
-    # Konversi estimasi_selesai dan tanggal_target_selesai ke objek datetime
-    estimasi_selesai = datetime.strptime(estimasi_selesai, '%Y-%m-%d')
-    tanggal_target_selesai = datetime.strptime(tanggal_target_selesai, '%Y-%m-%d')
-
-    # Bandingkan tanggal dan cetak hasilnya
-    if estimasi_selesai <= tanggal_target_selesai:
-        print("Tepat waktu")
-    else:
-        print("Terlambat")
-
 # Input estimasi waktu selesai proyek dan tanggal target selesai
 estimasi_selesai = input("Masukkan estimasi waktu selesai proyek (YYYY-MM-DD): ")
 tanggal_target_selesai = input("Masukkan tanggal target selesai (YYYY-MM-DD): ")
@@ -22,6 +11,8 @@ tanggal_target_selesai = input("Masukkan tanggal target selesai (YYYY-MM-DD): ")
 estimasi_selesai = datetime.strptime(estimasi_selesai, '%Y-%m-%d')
 tanggal_target_selesai = datetime.strptime(tanggal_target_selesai, '%Y-%m-%d')
 
-
-# Panggil fungsi cek_tepat_waktu
-cek_tepat_waktu(estimasi_selesai, tanggal_target_selesai)
+# Bandingkan tanggal dan cetak hasilnya
+if estimasi_selesai <= tanggal_target_selesai:
+    print("Tepat waktu")
+else:
+    print("Terlambat")
